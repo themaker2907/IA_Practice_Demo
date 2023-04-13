fac68b745b513aeaa2c2a183f6335886df5560f9
themaker2907 <55945594+themaker2907@users.noreply.github.com>
2023-04-13 11:38:39 +0800

    commit 3

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ const submitButton = document.getElementById('submit_button');
 let nameArr = []
 let ageArr = []
 
+getRandomUser();
+
+async function getRandomUser() {
+    const data = await fetch('https://randomuser.me/api/');
+    .then(response) => {
+        let fetchedData = response.json();
+        console.log(fetchedData);
+    });
+}
+
 console.log(form);
 
 
@@ -15,8 +25,10 @@ function clickHandler(e){
     e.preventDefault();
     let formData = new FormData(form);
 
-    nameArr.push(formData.get("name_input"));
-    ageArr.push(formData.get("age"));
+    // nameArr.push(formData.get("name_input"));
+    // ageArr.push(formData.get("age"));
+
+    addPerson(formData.get("name_input"), formData.get("age" ));
 
     viewNameArr();
     viewAgeArr();
@@ -32,3 +44,28 @@ function viewAgeArr(){
         console.log(item);
     })
 }
+
+const peopleContainer = document.querySelector('.person');
+
+
+function addPerson(name, age){
+    //personDiv is the container for name and age div
+    const personDiv = document.createElement('div');
+    personDiv.className = 'person';
+    peopleContainer.appendChild(personDiv);
+
+    //create and append nameDiv to personDiv
+    const nameDiv = document.createElement('div');
+    nameDiv.innerHTML = `Name: ${name}`;
+    personDiv.appendChild(nameDiv)
+
+    //create and append ageDiv to personDiv
+
+    const ageDiv = document.createElement('div');
+    ageDiv.innerHTML = `age: ${age}`;
+    personDiv.appendChild(ageDiv);
+}
+// addPerson("jason", 16);
+// addPerson("ethan", 16);
+// addPerson("sam", 16);
+// addPerson("kevin the monke", 16);
